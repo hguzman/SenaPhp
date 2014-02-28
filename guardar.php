ed58e9eb187a6b5ae0f9d591580150b52082f5f8
Henry Miguel Guzmán Escorcia <hguzman10@gmail.com>
2014-02-28 11:43:26 -0500

    Prueba Insertar Datos en Tabla

---
+++ guardar.php
@@ -0,0 +1,18 @@
+<?php
+
+$ced=$_POST["ced"];
+$nom=$_POST["nombre"];
+$dir=$_POST['dir'];
+$tel=$_POST['tel'];
+$cel=$_POST['cel'];
+
+$db = mysql_connect("localhost", "root","usrio01");
+mysql_select_db("sena",$db);
+
+$sql = "INSERT INTO personas value($ced,'$nom','$dir',$tel,$cel)";
+$result = mysql_query($sql);
+
+echo "¡GraciaS";
+
+?>varchar jajajajajaja
+el rpofesor nos va poner mala nota ya saben varchar
